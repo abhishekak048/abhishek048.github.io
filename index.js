ae92820ff41735e0769e44ce313ff311eb8128b5
Abhishek kumar <54079908+abhishekak048@users.noreply.github.com>
2023-09-04 13:11:25 +0530

    js change.

---
+++ index.js
@@ -0,0 +1,29 @@
+import { useEffect } from 'react';
+
+export default function IndexPage() {
+  useEffect(() => {
+    console.log('heyyaaa');
+    window.addEventListener(
+      'flutterInAppWebViewPlatformReady',
+      function (event) {
+        window.flutter_inappwebview
+          .callHandler('handlerFoo', 'hello bro')
+          .then(function (result) {
+            // print to the console the data coming
+            // from the Flutter side.;;
+            console.log(JSON.stringify(result));
+
+            window.flutter_inappwebview.callHandler(
+              'handlerFooWithArgs',
+              1,
+              true,
+              ['bar', 5],
+              { foo: 'baz' },
+              result
+            );
+          });
+      }
+    );
+  });
+  return <div>Hello World.</div>;
+}
